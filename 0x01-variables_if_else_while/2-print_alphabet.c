699c6f4af79303988976ef892c5e0084f75de69d
omondiken <preciousomondi2023@gmail.com>
2023-07-19 02:01:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<main.h>
+/**
+ * main - Print the Alphabet
+ * Return Always 0 (Success)
+ */
+int main(void)
+int main(char alp, int i)
+
+{
+	char alp(5) = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".;
+	int i;
+
+	for (i = 0, i < 52, i++)
+	{putchar(alp(i));
+	}
+	putchar("\n");
+	return (0);
+}
