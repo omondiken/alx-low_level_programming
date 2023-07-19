5d5cc6f55316384e11a329b4915dca8c25950a9d
omondiken <preciousomondi2023@gmail.com>
2023-07-19 02:56:59 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,15 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-int main(char alp, int i)
+
 
 {
-	char alp(5) = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".;
+	char alp(52) = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0, i < 52, i++)
-	{putchar(alp(i));
+	{
+	putchar(alp(i));
 	}
 	putchar("\n");
 	return (0);
