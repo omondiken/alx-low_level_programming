c87d5f793228294fb0138f336f8114bfe09b80cb
omondiken <preciousomondi2023@gmail.com>
2023-07-20 20:42:55 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<time.h>
+/**
+ * positive_or_negative - assign a random number to the variable i
+ * @i : integer
+ *
+ * Return: Always 0 (Success)
+*/
+#include<stdio.h>
+
+void positive_or_negative(int i)
+
+{
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+
+}
